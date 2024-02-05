a40820387d5cdff9abe9224177e32a306cc8f521
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2024-02-05 11:24:23 -0500

    feat(middleware): Add middleware to count how many times the app endpoint has been requested

--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := apiConfig{
+		fileServerHits: 0,
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
 
 	corsMux := middlewareCors(mux)
 
@@ -22,11 +27,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-
-	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
