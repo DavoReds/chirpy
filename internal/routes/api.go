602348162aeba31b90727bc18f914fe00f83add3
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2024-02-05 20:31:09 -0500

    Add validate Chirp endpoint

--- internal/routes/api.go
+++ internal/routes/api.go
@@ -1,14 +1,65 @@
 package routes
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/DavoReds/chirpy/internal/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func MountAPIEndpoints(apiCfg *middleware.ApiConfig, router *chi.Mux) {
 	apiRouter := chi.NewRouter()
-	apiRouter.Get("/healthz", HandlerReadiness)
+	apiRouter.Get("/healthz", handlerReadiness)
+	apiRouter.Post("/validate_chirp", handlerValidateChirp)
 	apiRouter.HandleFunc("/reset", apiCfg.HandleReset)
 
 	router.Mount("/api", apiRouter)
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+type validateChirpParams struct {
+	Body string `json:"body"`
+}
+
+type validateChirpResponse struct {
+	Valid bool `json:"valid"`
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := validateChirpParams{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, 200, validateChirpResponse{Valid: true})
 }
